d8d819b44568a19482bba8d8389b6d9df14c76a6
Lz-Rod <lz.rod@outlook.com>
2023-03-31 20:04:24 -0300

    Criado projeto Unity e primeiro script C# hello world

---
+++ Assets/Scripts/Aula01.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Aula01 : MonoBehaviour
+{
+    int number;
+    float otherNumber;
+    GameObject go;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Hello World!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
